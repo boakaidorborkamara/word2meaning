d4c8e4ffc51a5f0333b89c147066f8383c7058d8
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-11-30 12:47:46 +0000

    re-organize project directory

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,31 +1,23 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-      publicPath: "dist",
-      filename: 'bundle.js',
-    },
+  entry: "./src/index.js",
+  output: {
+    publicPath: "dist",
+    filename: "bundle.js",
+  },
 
-    plugins: [
-      new HtmlWebpackPlugin({
-          template: './src/index.html',
-          filename: 'index.html'
-      })
-    ],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+  ],
 
-    module: {
-      rules: [
-          { test: /\.css$/,
-            use: ['style-loader','css-loader'] 
-          }
-      ]
-    },
+  module: {
+    rules: [{ test: /\.css$/, use: ["style-loader", "css-loader"] }],
+  },
 
-  
-
-    
-    
-    mode: "development"
+  mode: "development",
 };
